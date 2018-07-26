ecfbe61c072c4660dc5127d3cdd792578248d708
thandile <thandilexipu@gmail.com>
2018-07-26 22:45:01 +0200

    TST: adds more tests for ERC20 implementation

--- TikkunToken/test/TestTikkunToken.js
+++ TikkunToken/test/TestTikkunToken.js
@@ -7,53 +7,77 @@ contract('TikkunToken', function(accounts) {
       var token;
       return TikkunToken.deployed().then(function(instance){
        token = instance;
-       return token.totalSupply.call();
+       return token.totalSupply();
       }).then(function(result){
-       assert.equal(result.toNumber(), 1000000, 'total supply is wrong');
+       assert.equal(result.toNumber(), 0, 'total supply is wrong');
       })
     });
-  });
 
-contract('TikkunToken', function(accounts) {
     it("should return the balance of token owner", function() {
         var token;
         return TikkunToken.deployed().then(function(instance){
         token = instance;
-        return token.balanceOf.call(accounts[0]);
+        return token.balanceOf(accounts[0]);
         }).then(function(result){
         assert.equal(result.toNumber(), 0, 'balance is wrong');
         })
     });
-});
 
-
-contract('TikkunToken', function(accounts) {
     it("should increase totalSupply", function() {
         var token;
         return TikkunToken.deployed().then(function(instance){
         token = instance;
-        return token.increaseSupply.call(1000000);
+        token.increaseSupply(1000000, accounts[0], {'from': accounts[0]});
+        return token.balanceOf(accounts[0]);
         }).then(function(result){
-        return token.totalSupply.call();
-      }).then(function(result){
-       assert.equal(result.toNumber(), 2000000, 'total supply is wrong');
+        assert.equal(result.toNumber(), 1000000, 'accounts[0] balance is wrong');
       })
     });
-});
 
-contract('TikkunToken', function(accounts) {    
     it("should transfer right token", function() {
         var token;
         return TikkunToken.deployed().then(function(instance){
         token = instance;
-        return token.transfer(accounts[1], 5);
+        return token.transfer(accounts[1], 500000, {'from': accounts[0]});
         }).then(function(){
-        return token.balanceOf.call(accounts[0]);
+        return token.balanceOf(accounts[0]);
         }).then(function(result){
         assert.equal(result.toNumber(), 500000, 'accounts[0] balance is wrong');
-        return token.balanceOf.call(accounts[1]);
+        return token.balanceOf(accounts[1]);
         }).then(function(result){
-        assert.equal(result.toNumber(), 5, 'accounts[1] balance is wrong');
+        assert.equal(result.toNumber(), 500000, 'accounts[1] balance is wrong');
         })
     });
+    it("should give accounts[1] authority to spend account[0]'s token", function() {
+        var token;
+        return TikkunToken.deployed().then(function(instance){
+        token = instance;
+        return token.approve(accounts[1], 200000);
+        }).then(function(){
+        return token.allowance(accounts[0], accounts[1]);
+        }).then(function(result){
+        assert.equal(result.toNumber(), 200000, 'allowance is wrong');
+        return token.transferFrom(accounts[0], accounts[2], 200000, {from: accounts[1]});
+        }).then(function(){
+        return token.balanceOf(accounts[0]);
+        }).then(function(result){
+        assert.equal(result.toNumber(), 300000, 'accounts[0] balance is wrong');
+        return token.balanceOf(accounts[1]);
+        }).then(function(result){
+        assert.equal(result.toNumber(), 500000, 'accounts[1] balance is wrong');
+        return token.balanceOf(accounts[2]);
+        }).then(function(result){
+        assert.equal(result.toNumber(), 200000, 'accounts[2] balance is wrong');
+        })
+    });
+
+    it("should show the transfer event", function() {
+        var token;
+        return TikkunToken.deployed().then(function(instance){
+          token = instance;
+          return token.transfer(accounts[1], 100000);
+        }).then(function(result){
+          console.log(result.logs[0].event)
+        })
+      });
 });
